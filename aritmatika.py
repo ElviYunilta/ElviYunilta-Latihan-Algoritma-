4612dcc5235490b2ae1ed88cdbe28df64660670d
ElviYunilta <elviyunilta@gmail.com>
2024-10-23 23:13:57 +0800

    Add files via upload

---
+++ aritmatika.py
@@ -0,0 +1,18 @@
+a = 8
+b = 4
+
+perkalian = a * b
+print( perkalian) 
+# outputnya 32
+
+pembagian = a / b
+print(pembagian)
+#outputnya 2
+
+pertambahan = a + b
+print(pertambahan)
+#outputnya 12
+
+pengurangan = a - b
+print(pengurangan)
+#outputnya 4
