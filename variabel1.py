1f5336364fdae72ff0293639e1df0a12d4b71d35
ElviYunilta <elviyunilta@gmail.com>
2024-10-30 11:31:08 +0800

    Add files via upload

---
+++ variabel1.py
@@ -0,0 +1,32 @@
+#pembuatan variabel pyton dan menampilkan hasilnya
+
+nama = "ayam"
+harga = 100
+hasil = True
+
+print(nama)
+print(harga)
+print(hasil)
+
+# mengubah nilai variabel
+nama = "bebek"
+harga = 200
+hasil = False
+
+print(nama)
+print(harga)
+print(hasil)
+
+#foo/contoh fungsi sederhana
+foo = "hellow word"
+print(foo)
+foo = 10
+print(foo)
+foo = True
+print(foo)
+
+#nilai konstanta
+bulan = "april"
+print(bulan)
+nilaiPI = 3.14
+print(nilaiPI)
