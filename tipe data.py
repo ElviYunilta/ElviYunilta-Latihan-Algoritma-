57cac7be9aaff6df8d7fb69be05a416b4af26da0
ElviYunilta <elviyunilta@gmail.com>
2024-10-30 12:04:38 +0800

    Add files via upload

---
+++ tipe data.py
@@ -0,0 +1,29 @@
+#tipe data string
+a = "harimau"
+print("ini adalah string" , a)
+print("-bertipe :", type(a))
+
+#tipe data Integer
+b = 120
+print("ini adalah integer" , b)
+print("-bertipe :", type(b))
+
+#tipe data Float
+c = 12.3
+print("ini adalah Float" , c)
+print("-bertipe :", type(c))
+
+#tipe data Comlex Number
+d = 1 + 2j
+print("ini adalah compex float " , d)
+print("-bertipe :",type(d))
+
+#tipe data boolean
+e = True
+print("ini adalah boolean" , e)
+print("-bertipe", type(e))
+
+#tipe data List
+f = ["satu","dua","tiga","satu"]
+print("data :", f)
+print("-bertipe :", type (f))
