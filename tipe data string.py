2063b0e5fdba0ec5b5902274386417eee37ebe68
ElviYunilta <elviyunilta@gmail.com>
2024-10-30 12:43:05 +0800

    Add files via upload

---
+++ tipe data string.py
@@ -0,0 +1,24 @@
+#contoh tipe data string ada tiga
+
+#penggunaan titik satu (')
+a = 'menggunakan titik satu'
+print("-hasil:", a)
+
+#penggunaan titik dua ("")
+print('')
+b = "menggunakan titik dua"
+print("-hasil",b)
+
+#penggunaan titik satu sebanyaj tiga kali (''')
+print('')
+c = "menggunakan titik satu sebanyak tiga kali"
+print("-hasil",c)
+
+#operasi penyambungan tipe data string
+print('')
+a = 'ayo kita'
+b = " belajar"
+c = ''' coding'''
+d = a+b+c
+
+print (d)
