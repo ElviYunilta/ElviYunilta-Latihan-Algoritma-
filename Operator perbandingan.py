99eb5654f7178f1eb4b883183cab826beb319a50
ElviYunilta <elviyunilta@gmail.com>
2024-10-31 23:15:30 +0800

    Add files via upload

---
+++ Operator perbandingan.py
@@ -0,0 +1,15 @@
+	
+a= 7
+b = 10
+ 
+ 
+print('a =', a)
+print('b =', b)
+print('\n')
+ 
+print('a == b hasilnya', a == b)
+print('a != b hasilnya', a != b)
+print('a > b  hasilnya', a > b)
+print('a < b  hasilnya', a < b)
+print('a >= b hasilnya', a >= b)
+print('a <= b hasilnya', a <= b)
