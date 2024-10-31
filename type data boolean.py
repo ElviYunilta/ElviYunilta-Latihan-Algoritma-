3f9f6a933456b247ec0218c81b431cff2d72f342
ElviYunilta <elviyunilta@gmail.com>
2024-10-31 22:34:50 +0800

    Add files via upload

---
+++ type data boolean.py
@@ -0,0 +1,23 @@
+#type data booolean adalah tipe data yang memiliki dua nilai yaitu true/flase
+#tipe data boolean dengan nilai true and false
+a = True
+b = False
+print(a)
+print(b)
+
+#contoh perbandingan
+a = 5 > 2
+print(a)
+b = 10 < 2
+print(b)
+
+#logika If
+a = 2
+b = 4
+
+if a < b:
+    print("isi variabel a lebih kecil dari variabel b")
+elif a > b:
+    print("isi variabel a lebih besar dari variabel b")
+else:
+    print("isi variabel a sama dengan b")
