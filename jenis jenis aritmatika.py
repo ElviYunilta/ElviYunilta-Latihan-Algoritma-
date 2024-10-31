c4dadf4bc2ea5aa773b872036a74ae5726715a9c
ElviYunilta <elviyunilta@gmail.com>
2024-10-31 23:11:10 +0800

    Add files via upload

---
+++ jenis jenis aritmatika.py
@@ -0,0 +1,13 @@
+#jenis operator aritmatika
+
+a = 20
+b = 6
+
+
+print('a + b =', a + b)  # Penjumlahan
+print('a - b =', a - b)  # Pengurangan
+print('a * b =', a * b)  # Perkalian
+print('a / b =', a / b)  # Pembagian
+print('a // b =', a // b)  # Pembagian bulat
+print('a % b =', a % b)  # Sisa bagi
+print('a ** b =', a ** b)  # Pangkatan
