3a2465af48cb7f6d61eadc523fbdd29a5e9244df
ElviYunilta <elviyunilta@gmail.com>
2024-10-31 23:34:25 +0800

    Add files via upload

---
+++ operator assignment.py
@@ -0,0 +1,12 @@
+g = 7
+h = 4
+h = h + 1
+i = g + h
+j = i + i + g
+e = (i + j) * g
+
+print('Isi variabel g:', g)
+print('Isi variabel h:', h)
+print('Isi variabel i:', i)
+print('Isi variabel j:', j)
+print('Isi variabel e:', e)
