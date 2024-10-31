d52ac671773f7d0ab479854bb20fa348f7b754f3
ElviYunilta <elviyunilta@gmail.com>
2024-10-31 22:51:10 +0800

    Add files via upload

---
+++ type data dictionary.py
@@ -0,0 +1,9 @@
+# Dictionary untuk menyimpan warna buah
+buah = {
+    "apel": "merah",
+    "pisang": "kuning"
+}
+
+# Mencetak warna buah
+print(buah["apel"])    # Output: merah
+print(buah["pisang"])  # Output: kuning
