ba525379b37f09645f634c3e33c82d4518703d1c
ElviYunilta <elviyunilta@gmail.com>
2024-11-04 19:34:50 +0800

    Add files via upload

---
+++ percabangan Kondisi If Else If Bahasa Python     .py
@@ -0,0 +1,11 @@
+# Percabangan Kondisi If Else If Bahasa Python
+
+
+nilai = 85
+
+if nilai >= 90:
+    print("Nilai A")
+elif nilai >= 80:
+    print("Nilai B")
+else:
+    print("Nilai C atau di bawahnya")
