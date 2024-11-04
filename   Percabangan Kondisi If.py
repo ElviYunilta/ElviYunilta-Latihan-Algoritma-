3645776a00594fd2f4d8046a2ae0551237f6f8ab
ElviYunilta <elviyunilta@gmail.com>
2024-11-04 19:27:08 +0800

    Add files via upload

---
+++   Percabangan Kondisi If.py
@@ -0,0 +1,13 @@
+#Percabangan Kondisi If Bahasa Python
+
+
+umur = 20
+
+if umur < 13:
+    print("Anak-anak")
+elif 13 <= umur < 18:
+    print("Remaja")
+elif 18 <= umur < 60:
+    print("Dewasa")
+else:
+    print("Lansia")
